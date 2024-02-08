b201788d0e20d0289b272565d4854b48637c219e
shakhruz <ismoilovshaxruz775@gmail.com>
2024-02-09 02:13:55 +0500

    caroucel card footer main full

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,60 +4,60 @@ import footerLogo from "../../assets/images/header_logo.png";
 import { PiTelegramLogoBold } from "react-icons/pi";
 import { FaInstagram } from "react-icons/fa";
 import { FiFacebook } from "react-icons/fi";
-import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="container">
       <footer className="footer">
         <div className="footer_content">
-          <div className="footer_left">
-            <div className="footer_card">
-              <p className="footer_card_title">Aloqa uchun:</p>
-              <div className="footer_card_contact">
-                <p className="footer_card_text">Telefon raqamlari: </p>
-                <div className="footer_number_div">
-                  <a className="footer_card_number" href="tel:+998995926600">
+          <div className="footer_left_content">
+            <div className="footer_left_content_one">
+              <p className="footer_left_title_one">Aloqa uchun:</p>
+              <div className="footer_left_phone_div">
+                <p className="footer_left_phone_title">Telefon raqamlari: </p>
+                <div className="footer_left_phone_number_div">
+                  <a className="footer_phone_number" href="tel:+998995926600">
                     +998 99 592 66 00
                   </a>
-                  <a className="footer_card_number" href="tel:+998995824000">
+                  <a className="footer_phone_number" href="tel:+998995824000">
                     +998 99 582 40 00
                   </a>
                 </div>
               </div>
             </div>
-            <div className="footer_card">
-              <p className="footer_card_title">Manzil:</p>
-              <p className="footer_card_location_text">
-                Toshkent sh. Yunusobot <br /> tumani, Osiyo 6.
-              </p>
-            </div>
-          </div>
-          <Link to="/" className="footer_card">
-            <img
-              className="location_images"
-              src={LocationImages}
-              alt="images"
-            />
-          </Link>
-          <div className="footer_card">
-            <Link to="/">
-              <img className="footer_logo" src={footerLogo} alt="footer_logo" />
-            </Link>
 
-            <div className="footer_card_icons">
-              <a className="footer_icon" href="/">
-                <PiTelegramLogoBold />
-              </a>
-              <a className="footer_icon" href="/">
-                <FaInstagram />
-              </a>
-              <a className="footer_icon" href="/">
-                <FiFacebook />
+            <div className="footer_left_content_two">
+              <p className="footer_left_title_one">Manzil:</p>
+              <a className="footer_location_text" href="/">
+                Toshkent sh. Yunusobot tumani, Osiyo 6.
               </a>
             </div>
           </div>
-          <p className="footer_bottom_text">© 2024 All right reserved</p>
+
+          <div className="footer_right_content">
+            <img
+              className="footer_location_images"
+              src={LocationImages}
+              alt="footer_location"
+            />
+
+            <div className="footer_logo_div">
+              <img className="footer_logo" src={footerLogo} alt="logo" />
+              <div className="footer_icon_group">
+                <a className="footer_icons" href="/">
+                  <PiTelegramLogoBold />
+                </a>
+                <a className="footer_icons" href="/">
+                  <FaInstagram />
+                </a>
+                <a className="footer_icons" href="/">
+                  <FiFacebook />
+                </a>
+              </div>
+            </div>
+            <p className="footer_bottom_text">© 2024 All right reserved</p>
+          </div>
         </div>
+
         <p className="footer_bottom_text_media">© 2024 All right reserved</p>
       </footer>
     </div>
--- src/routes/more/card/Card.jsx
+++ src/routes/more/card/Card.jsx
@@ -1,7 +1,38 @@
 import "./Card.css";
-
+import card1 from "../../../assets/images/card1.png";
+import card2 from "../../../assets/images/card2.png";
+import card3 from "../../../assets/images/card3.png";
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="container">
+      <div className="card_content">
+        <div className="card">
+          <img src={card1} alt="card_images" className="card_images" />
+          <h2 className="card_title">Xavfsiz to'lov tizimi</h2>
+          <p className="card_text">
+            Istalgan toʻlov usuli mavjud! To’lovni kitobni qabul qilgandan keyin
+            amalga oshirish imkoniyati! Nasiyaga xarid qilish mumkun!
+          </p>
+        </div>
+        <div className="card">
+          <img src={card2} alt="card_images" className="card_images" />
+          <h2 className="card_title">Bepul yetkazib berish</h2>
+          <p className="card_text">
+            Istalgan toʻlov usuli mavjud! To’lovni kitobni qabul qilgandan keyin
+            amalga oshirish imkoniyati! Nasiyaga xarid qilish mumkun!
+          </p>
+        </div>
+        <div className="card">
+          <img src={card3} alt="card_images" className="card_images" />
+          <h2 className="card_title">100% Kafolat</h2>
+          <p className="card_text">
+            Istalgan toʻlov usuli mavjud! To’lovni kitobni qabul qilgandan keyin
+            amalga oshirish imkoniyati! Nasiyaga xarid qilish mumkun!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
